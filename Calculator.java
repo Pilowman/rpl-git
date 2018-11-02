06ac54753dd3422d2f602d505b002d6527b6db1e
JOSEPH FEBRYANO NATHAN <i16007@ftis>
2018-11-02 13:42:36 +0700

    initial commit

---
+++ Calculator.java
@@ -0,0 +1,27 @@
+public class Calculator{
+    public Calculator(){
+
+    }
+
+    public float plus(floata,floatb){
+
+
+        return 0;
+    }
+    public float minus(floata,floatb){
+
+
+        return 0;
+    }
+    public float multiply(floata,floatb){
+
+
+        return 0;
+    }
+    public float division(floata,floatb){
+
+        
+        return 0;
+    }
+
+}
